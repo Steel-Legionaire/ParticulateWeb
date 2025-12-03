9ab3f2ba46ca486d63e5f88a6b7a82d8d6ab974b
Vince <theros113@gmail.com>
2025-12-03 11:04:51 -0500

    Only requires x y app matrix

--- src/particles/solids/moveableSolids/sand.js
+++ src/particles/solids/moveableSolids/sand.js
@@ -2,8 +2,8 @@ import { Graphics } from "pixi.js";
 import MoveableSolid from './moveableSolid'
 
 class Sand extends MoveableSolid{
-    constructor(x, y, isFlammable, isDestructable, toughness, speed, app, matrix){
-        super(x, y, isFlammable, isDestructable, toughness, speed, app, matrix)
+    constructor(x, y, app, matrix){
+        super(x, y, false, true, 5, 0, app, matrix)
 
         let colors = [0xDBD49D, 0xC7C089, 0xDED7A1, 0xCCC78F, 0xE9E9AE];
 
