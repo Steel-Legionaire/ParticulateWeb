7be8359f4f0b0be8602a3b3640fa0a90164fe58a
Steel Legionnaire <162994122+Steel-Legionaire@users.noreply.github.com>
2025-12-04 21:52:13 -0500

    Update PIXI.js import to use CDN link

--- src/particles/particle.js
+++ src/particles/particle.js
@@ -1,4 +1,4 @@
-import { Graphics } from "pixi.js";
+import {  Graphics } from "https://cdn.jsdelivr.net/npm/pixi.js@8/dist/esm/index.js";
 
 class Particle{
     constructor(x, y, isFlammable, isDestructable, toughness, speed, app, matrix){
